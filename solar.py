437fa252eb34e426ecd53737482f3133ba5ca7fb
mamedesigor <mamedesigor@gmail.com>
2024-02-05 21:56:55 -0400

    add login function

--- solar.py
+++ solar.py
@@ -1,8 +1,26 @@
-""" Helper script for downloading data from sems portal e plot graphs """
+""" Helper script for downloading data from sems portal and ploting graphs """
+import json
+import requests
+from config import args
+
+API_URL = "https://www.semsportal.com/api/"
+
+headers = {"Token": "{'version': 'v2.1.0', 'client': 'ios', 'language': 'en'}"}
+
+
+def login() -> None:
+    payload = {
+        "account": args.get("gw_account"),
+        "pwd": args.get("gw_password"),
+    }
+    url = API_URL + "v1/Common/CrossLogin"
+    response = requests.post(url, headers=headers, json=payload)
+    Token = response.json().get("data")
+    headers["Token"] = json.dumps(Token)
 
 
 def main() -> None:
-    print("hello ufms")
+    login()
 
 
 if __name__ == "__main__":
