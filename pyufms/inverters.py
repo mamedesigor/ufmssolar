1cdc789500df49d1d9047d49ae9512297f034125
mamedesigor <mamedesigor@gmail.com>
2024-02-07 23:13:16 -0400

    add inverter enum for identification

---
+++ pyufms/inverters.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Inverter(Enum):
+    S1_BL20_1 = 1
+    S1_BL20_2 = 2
+    S1_BL11 = 3
+    S1_BL8 = 4
+    S1_BL15 = 5
+    S1_BL5 = 6
+    S1_BL7 = 7
+    S1_BL55 = 8
+    S1_BL4 = 9
+    S1_BL18_1 = 10
+    S1_BL18_2 = 11
+    S1_BL19 = 12
+    S1_BL13_1 = 13
+    S1_BL13_2 = 14
+    S1_BL14 = 15
